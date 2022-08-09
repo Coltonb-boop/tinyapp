101b7ddfeda6d84a20b164beb7082cf8790d28ec
Coltonb-boop <coltonbitz@gmail.com>
2022-08-08 20:46:48 -0600

    shortURL now redirecting to longURL

--- express_server.js
+++ express_server.js
@@ -32,7 +32,9 @@ app.post('/urls', (req, res) => {
   // console.log('from post /urls', req.body);
   let newShort = generateRandomString()
   urlDatabase[newShort] = req.body.longURL;
+
   const templateVars = { id: newShort, longURL: req.body.longURL };
+
   res.render('urls_show', templateVars);
 });
 
@@ -56,6 +58,11 @@ app.get('/urls', (req, res) => {
 app.get('/urls/new', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_new', templateVars);
+});
+const inspect = require('util').inspect;
+app.get('/u/:id', (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get('/urls/:id', (req, res) => {
